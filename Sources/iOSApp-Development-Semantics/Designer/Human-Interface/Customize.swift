2232d5053e531af13bed2aafb3ee2d218d20762e
Paper <fredchowdz@gmail.com>
2020-12-14 11:54:12 +0800

    .

--- Sources/iOSApp-Development-Semantics/Designer/Human-Interface/Customize.swift
+++ Sources/iOSApp-Development-Semantics/Designer/Human-Interface/Customize.swift
@@ -1,1 +1,29 @@
+Arrange TVShows on maps:
 
+        Projection
+Junior  Admire      Senior
+
+
+
+resource
+------------
+believe taste implement
+    soil
+        
+            
+    
+------------
+Customer gets to choose
+
+    
+* 第三者
+* relationship: 如果单方就可以，就没有suffer了
+    )
+
+
+
+Language
+* lingerie
+* Stage Director
+* Tailor
+* Magazine
