d73005b5338de02fbf18fdf8577c8588efb86ffd
Paper <fredchowdz@gmail.com>
2020-12-19 15:08:32 +0800

    .

--- Sources/iOSApp-Development-Semantics/Engineer/TrialAndError/TrialAndError.swift
+++ Sources/iOSApp-Development-Semantics/Engineer/TrialAndError/TrialAndError.swift
@@ -4,6 +4,7 @@ import Elo
 // 实现Idea时，思维跨大步子会造成：焦虑/失控感/无意义感
     // 基于别人的工作做一点创新，既而带动别人支持你
     // 做细节的时候，感到很慢，感到花这些时间没有价值，但这恰恰是锻炼技能的时候，反映了我还远未成熟，要把每天放慢专注于每一天
+// 进入新领域时，会感觉到很杂被overwhelm了
 
 struct Experience_aheadof_Mastery: Approach, Shape_Reality {
     var content = SeekHelp_from_newApproaches_and_Perspectives()
